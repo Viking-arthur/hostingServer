fc476d2ba45311551b6980c4c0e6f1c5a81faa6c
Viking-arthur <sandeepvenu.g@gmail.com>
2024-10-09 12:58:50 +0530

    server Changes

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get('/api/message', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
